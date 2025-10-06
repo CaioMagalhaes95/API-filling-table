da090f986fa7c2d1323a28ceb970ccf0d52311c1
Caio Magalhães <magalhaescaio7@gmail.com>
2025-10-05 22:37:14 -0300

    toast added, table to fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import { UploadProvider } from './components/components'
 import TableTest from './table/table'
 import InsertFile from './upload/fileupload'
 import { Stack } from '@mui/material'
-
+import { Toast } from "primereact/toast";
+import {useRef} from "react";
 
 function App() {
-  
+  const toast = useRef<Toast | null>(null); 
+
+  // Função que exibe mensagens do Toast
+  const showToast = (message: string, type: "success" | "info" | "warn" | "error" = "info") => {
+    (toast.current as Toast).show({
+      severity: type,  // success | info | warn | error
+      summary: type.toUpperCase(),
+      detail: message,
+      life: 4000,
+    });
+  };
 
   return (
     <>
     <UploadProvider>
       <Stack direction="row" spacing={2}>
-        <InsertFile></InsertFile>
+        <Toast ref={toast}/>
+        <InsertFile showToast={showToast}></InsertFile>
         <TableTest></TableTest>
       </Stack>
     
